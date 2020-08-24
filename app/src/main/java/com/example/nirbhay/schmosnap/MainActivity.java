db5d780db1bb18537162d88fda9ce29ad4d90c5c
nibchee <51364680+nibchee@users.noreply.github.com>
2020-08-25 01:38:54 +0530

    Initaially Login & Sign Up Layout wih Profile Layout is ready

---
+++ app/src/main/java/com/example/nirbhay/schmosnap/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.nirbhay.schmosnap;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity
+{
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private RecyclerView postList;
+    private Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mToolbar=(Toolbar)findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Home");
+
+        drawerLayout=(DrawerLayout) findViewById(R.id.drawable_layout);
+        actionBarDrawerToggle=new ActionBarDrawerToggle(MainActivity.this,drawerLayout,R.string.drawer_open,R.string.drawer_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        navigationView=(NavigationView)findViewById(R.id.navigation_view);
+        View navView=navigationView.inflateHeaderView(R.layout.navigation_header);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem)
+            {
+                UserMenuSelector(menuItem);
+                return false;
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item)
+    {
+        if(actionBarDrawerToggle.onOptionsItemSelected(item))
+        {
+            return  true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void UserMenuSelector(MenuItem item)
+    {
+        switch(item.getItemId())
+        {
+            case R.id.nav_profile:
+                Toast.makeText(this,"Profile",Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_home:
+                Toast.makeText(this,"Home ",Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_friends:
+                Toast.makeText(this,"Friend List",Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_message:
+                Toast.makeText(this,"Messages",Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_settings:
+                Toast.makeText(this,"Settings",Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_Logout:
+                Toast.makeText(this,"Logout",Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+}
