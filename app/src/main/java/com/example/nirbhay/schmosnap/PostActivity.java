03cc99727d9082afdad147b7f5e4da03e083cfcc
nibchee <51364680+nibchee@users.noreply.github.com>
2020-08-27 02:21:04 +0530

    In Post Activity ,User now can select the post & post get saved on Firebase Storage

--- app/src/main/java/com/example/nirbhay/schmosnap/PostActivity.java
+++ app/src/main/java/com/example/nirbhay/schmosnap/PostActivity.java
@@ -1,14 +1,174 @@
 package com.example.nirbhay.schmosnap;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
-public class PostActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class PostActivity extends AppCompatActivity
+{
+     private Toolbar mToolbar;
+
+     private ImageButton SelectPostImage;
+     private Button UpdatePostButton;
+     private EditText PostDescription;
+
+     private static  final int Gallery_Pick=1;
+     private Uri ImageUri;
+     private String Description;
+
+     private StorageReference PostImagesReference;
+
+     private String  saveCurrentDate,saveCurrentTime,postRandomName;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        PostImagesReference= FirebaseStorage.getInstance().getReference();
+
+        SelectPostImage=(ImageButton)findViewById(R.id.select_post_image);
+        UpdatePostButton=(Button)findViewById(R.id.update_post_button);
+        PostDescription=(EditText)findViewById(R.id.post_description);
+
+        mToolbar=(Toolbar)findViewById(R.id.update_post_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("Update Post");
+
+        SelectPostImage.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                OpenGallery();
+
+            }
+        });
+
+        UpdatePostButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                ValidatePostInfo();
+
+            }
+        });
+    }
+
+    private void ValidatePostInfo()
+    {
+        Description=PostDescription.getText().toString();
+
+        if(ImageUri == null)
+        {
+            Toast.makeText(this,"Please Select Post Image....",Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(Description))
+        {
+            Toast.makeText(this,"Please write something about your image....",Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            StoringImageToFirebaseStorage();
+        }
+    }
+
+    //to Store Post Image uniquely using time as two posts can be same also,more efficient than generating random keys
+    private void StoringImageToFirebaseStorage()
+    {
+        //Date
+        Calendar calFordDate=Calendar.getInstance();
+        SimpleDateFormat currentDate=new SimpleDateFormat("dd-MMMM-yyyy");
+        saveCurrentDate=currentDate.format(calFordDate.getTime());
+
+//Time
+        Calendar calFordTime=Calendar.getInstance();
+        SimpleDateFormat currentTime=new SimpleDateFormat("HH:mm");
+        saveCurrentTime=currentTime.format(calFordDate.getTime());
+
+        postRandomName=saveCurrentDate + saveCurrentTime;
+
+
+        StorageReference filepath=PostImagesReference.child("Post Images").child(ImageUri.getLastPathSegment()+ postRandomName+ ".jpg");
+
+        filepath.putFile(ImageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task)
+            {
+                if(task.isSuccessful())
+                {
+                    Toast.makeText(PostActivity.this,"Image Uplaoded Sucessfully to Storage..",Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    String message=task.getException().getMessage();
+                    Toast.makeText(PostActivity.this,"Error Occured:"+message,Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+    }
+
+    private void OpenGallery()
+    {
+        Intent galleyIntent=new Intent();
+        galleyIntent.setAction(Intent.ACTION_GET_CONTENT);
+        galleyIntent.setType("image/*");
+        startActivityForResult(galleyIntent,Gallery_Pick);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==Gallery_Pick && resultCode==RESULT_OK && data!=null)
+        {
+            ImageUri=data.getData();
+            SelectPostImage.setImageURI(ImageUri);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item)
+    {
+        int id=item.getItemId();
+           //for back button
+        if(id==android.R.id.home)
+        {
+            SendUserToMainActivity();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void SendUserToMainActivity()
+    {
+        Intent mainIntent=new Intent(PostActivity.this,MainActivity.class);
+        startActivity(mainIntent);
     }
 }
