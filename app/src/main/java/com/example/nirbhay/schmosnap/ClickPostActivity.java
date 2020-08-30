ca3a4a378faafde5d8d8bec9b52cd2823173ab6e
nibchee <51364680+nibchee@users.noreply.github.com>
2020-08-31 03:10:13 +0530

    Delete Post Button is activated .

--- app/src/main/java/com/example/nirbhay/schmosnap/ClickPostActivity.java
+++ app/src/main/java/com/example/nirbhay/schmosnap/ClickPostActivity.java
@@ -2,11 +2,15 @@ package com.example.nirbhay.schmosnap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -22,15 +26,21 @@ public class ClickPostActivity extends AppCompatActivity
     private ImageView PostImage;
     private TextView PostDescription;
     private Button DeletePostButton,EditPostButton;
-    private DatabaseReference  ClickPostRef;
 
-    private String PostKey;
+    private DatabaseReference  ClickPostRef;
+    private FirebaseAuth mAuth;
+
+    private String PostKey,currentUserID,databaseUserID,image,description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_click_post);
+
+        //to get current online user
+        mAuth=FirebaseAuth.getInstance();
+        currentUserID=mAuth.getCurrentUser().getUid();
 
         PostKey=getIntent().getExtras().get("PostKey").toString();
         ClickPostRef= FirebaseDatabase.getInstance().getReference().child("Posts").child(PostKey);
@@ -40,17 +50,32 @@ public class ClickPostActivity extends AppCompatActivity
         DeletePostButton=(Button)findViewById(R.id.delete_post_button);
         EditPostButton=(Button)findViewById(R.id.edit_post_button);
 
+
+        //if user clicks any posts in stsrting then edit,delete are invisibke
+        DeletePostButton.setVisibility(View.INVISIBLE);
+        EditPostButton.setVisibility(View.INVISIBLE);
+
         ClickPostRef.addValueEventListener(new ValueEventListener()
         {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot)
             {
-                String description=dataSnapshot.child("description").getValue().toString();
-                String image=dataSnapshot.child("postimage").getValue().toString();
+               if(dataSnapshot.exists())
+               {
+                   description=dataSnapshot.child("description").getValue().toString();
+                   image=dataSnapshot.child("postimage").getValue().toString();
+                   databaseUserID=dataSnapshot.child("uid").getValue().toString();
 
-                PostDescription.setText(description);
-                Picasso.get().load(image).into(PostImage);
+                   PostDescription.setText(description);
+                   Picasso.get().load(image).into(PostImage);
 
+                   if(currentUserID.equals(databaseUserID))
+                   {
+                       DeletePostButton.setVisibility(View.VISIBLE);
+                       EditPostButton.setVisibility(View.VISIBLE);
+                   }
+
+               }
             }
 
             @Override
@@ -58,5 +83,30 @@ public class ClickPostActivity extends AppCompatActivity
 
             }
         });
+
+        DeletePostButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                DeleteCurrentPost();
+
+            }
+        });
+    }
+
+    private void DeleteCurrentPost()
+    {
+        ClickPostRef.removeValue();
+        SendUserToMAinActivity();
+        Toast.makeText(this,"Post has ben deleted",Toast.LENGTH_SHORT).show();
+    }
+
+
+    private void SendUserToMAinActivity()
+    {
+        Intent mainIntent=new Intent(ClickPostActivity.this,MainActivity.class);
+        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(mainIntent);
+        finish();
     }
 }
