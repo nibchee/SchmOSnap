6613d090f40a02c9de5d13d5eeef1997b25e9473
nibchee <51364680+nibchee@users.noreply.github.com>
2020-08-31 04:39:44 +0530

    Settings Activity Layout is created

--- app/src/main/java/com/example/nirbhay/schmosnap/ClickPostActivity.java
+++ app/src/main/java/com/example/nirbhay/schmosnap/ClickPostActivity.java
@@ -1,11 +1,15 @@
 package com.example.nirbhay.schmosnap;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -75,6 +79,16 @@ public class ClickPostActivity extends AppCompatActivity
                        EditPostButton.setVisibility(View.VISIBLE);
                    }
 
+                   //For editing Post
+                   EditPostButton.setOnClickListener(new View.OnClickListener()
+                   {
+                       @Override
+                       public void onClick(View v)
+                       {
+                          EditCurrentPost(description);
+                       }
+                   });
+
                }
             }
 
@@ -92,6 +106,39 @@ public class ClickPostActivity extends AppCompatActivity
 
             }
         });
+    }
+
+    private void EditCurrentPost(String description)
+    {
+        AlertDialog.Builder builder=new AlertDialog.Builder(ClickPostActivity.this);
+        builder.setTitle("Edit Post:");
+
+        final EditText inputField=new EditText(ClickPostActivity.this);
+        inputField.setText(description);
+        builder.setView(inputField);
+
+        builder.setPositiveButton("Update", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                ClickPostRef.child("description").setValue(inputField.getText().toString());
+                Toast.makeText(ClickPostActivity.this,"Post Updated sucessfully....",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                dialog.cancel();
+
+            }
+        });
+
+        Dialog dialog=builder.create();
+        dialog.show();
+        dialog.getWindow().setBackgroundDrawableResource(android.R.color.holo_green_dark);
     }
 
     private void DeleteCurrentPost()
---
+++ app/src/main/java/com/example/nirbhay/schmosnap/SettingsActivity.java
@@ -0,0 +1,14 @@
+package com.example.nirbhay.schmosnap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
